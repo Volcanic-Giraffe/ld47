3fa1e62cb12d442333f24df300075a0741304742
A333 <arz.giyan@gmail.com>
2020-10-03 19:05:29 +0300

    Enemy, damageable

---
+++ Assets/Damageable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    public float Health = 20;
+
+    public void Damage(GameObject who, float amount)
+    {
+        Debug.Log($"Damaging {gameObject.name} with {who.name} by {amount}.");
+        Health -= amount;
+        if (Health <= 0) Destroy(gameObject);
+    }
+}
---
+++ Assets/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float Damage = 5;
+    public GameObject Explosion;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var damageable = collision.gameObject.GetComponent<Damageable>();
+        if(damageable != null)
+        {
+            damageable.Damage(this.gameObject, Damage);
+        }
+        if (Explosion != null) Instantiate(Explosion, this.transform.position, Quaternion.identity);
+        
+        Destroy(gameObject);
+    }
+}
