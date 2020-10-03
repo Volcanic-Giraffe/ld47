c413e331a3815e93c4c0b34d788bdcd893cbb458
Ruslan Kibets <ruslan.kibets@gmail.com>
2020-10-03 16:43:00 -0700

    EnemySentry

---
+++ Assets/Scripts/Enemies/EnemyTurret.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurret : MonoBehaviour
+{
+    public float turnSpeed;
+    public float visionRadius;
+    public float readyToAttackAngle;
+
+    public Cannon cannon;
+    
+    private Rigidbody _rig;
+
+    private GameObject _player;
+    
+    void Start()
+    {
+        _rig = GetComponent<Rigidbody>();
+
+        FindPlayer();
+    }
+
+
+    private void FixedUpdate()
+    {
+        AttackPlayer();
+    }
+
+    private void AttackPlayer()
+    {
+        if (_player == null) return;
+        var distance = Vector3.Distance(transform.position, _player.transform.position);
+        if (distance > visionRadius) return;
+        
+        var localTarget = transform.InverseTransformPoint(_player.transform.position);
+     
+        var angle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
+
+        if (angle < readyToAttackAngle) cannon.Fire();
+        
+        var eulerAngleVelocity = new Vector3 (0, angle, 0);
+        var deltaRotation = Quaternion.Euler(eulerAngleVelocity * (Time.deltaTime * turnSpeed));
+
+        _rig.MoveRotation(_rig.rotation * deltaRotation);
+    }
+    
+    private void FindPlayer()
+    {
+        if (_player == null)
+        {
+            var objs = GameObject.FindGameObjectsWithTag("Hero");
+
+            if (objs != null && objs.Length > 0)
+            {    
+                _player = objs[0];
+            }
+        }
+    }
+}
