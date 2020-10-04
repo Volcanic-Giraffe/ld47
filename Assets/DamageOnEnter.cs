247bd0ef216a8197d0bdffe023d01540e4d40189
A333 <arz.giyan@gmail.com>
2020-10-04 13:50:21 +0300

    Turret corpses

---
+++ Assets/DamageOnEnter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DamageOnEnter : MonoBehaviour
+{
+    public float Damage = 5;
+    private void OnTriggerEnter(Collider other)
+    {
+        var damageable = other.GetComponent<Damageable>();
+
+        if (damageable != null)
+        {
+            damageable.Damage(gameObject, Damage);
+        }
+    }
+}
---
+++ Assets/Exploding.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Damageable))]
+public class Exploding : MonoBehaviour
+{
+
+    public GameObject Explosion;
+
+    void Start()
+    {
+        GetComponent<Damageable>().OnDie += Exploding_OnDie;
+    }
+
+    private void Exploding_OnDie()
+    {
+        Instantiate(Explosion, this.transform.position, Quaternion.identity);
+    }
+
+}
