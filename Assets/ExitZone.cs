277a5188e107ef97ee85fa4c754c49871f969a55
A333 <arz.giyan@gmail.com>
2020-10-05 22:29:45 +0300

    Exit zome

---
+++ Assets/ExitZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitZone : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Hero")
+        {
+            // TOdO EXIT
+            Destroy(this.gameObject);
+        }
+    }
+}
