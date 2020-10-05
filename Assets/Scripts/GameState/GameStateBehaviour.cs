0fa580568bd3a900ceca34237ae477d58df3990c
A333 <arz.giyan@gmail.com>
2020-10-05 17:47:03 +0300

    Fixbugs

--- Assets/Scripts/GameState/GameStateBehaviour.cs
+++ Assets/Scripts/GameState/GameStateBehaviour.cs
@@ -13,7 +13,15 @@ namespace GameState
 
         private void Awake()
         {
-            _hero = GameObject.FindGameObjectWithTag("Hero");
+            var hs = GameObject.FindGameObjectsWithTag("Hero");
+            foreach (var he in hs)
+            {
+                if (he.GetComponent<Damageable>() != null)
+                {
+                    _hero = he;
+                    break;
+                }
+            }
             _heroHealth = _hero.GetComponent<Damageable>();
             _state = GameState.GetInstance();
             _prevHeroPosition = _hero.transform.position;
@@ -36,13 +44,13 @@ namespace GameState
             if (_state == null || _hero == null) return;
 
             Handles.Label(_hero.transform.position, "" + SectorUtils.PositionToSectorIdx(_hero.transform.position));
-            
+
             var colors = new Color[]
             {
                 Color.green, Color.blue, Color.red, Color.magenta
             };
             var sectorAngle = 15;
-            
+
             for (int i = 0; i < 360; i += sectorAngle)
             {
                 var point = new Vector3(
@@ -53,7 +61,7 @@ namespace GameState
                 var loop = _state.GetLoopByIdx(SectorUtils.PositionToSectorIdx(point));
                 var color = colors[loop % colors.Length];
                 Handles.color = new Color(color.r, color.g, color.b, 0.1f);
-                Handles.DrawSolidArc(Vector3.zero, Vector3.up,  point, sectorAngle, 4f);
+                Handles.DrawSolidArc(Vector3.zero, Vector3.up, point, sectorAngle, 4f);
                 Handles.color = new Color(0, 0, 0, 0.5f);
 
                 Handles.Label(point, "Idx " + SectorUtils.PositionToSectorIdx(point) + " Loop " + loop);// + " ang = " + Mathf.Rad2Deg *Mathf.Atan2(-point.y, point.x) + "/" + i);
