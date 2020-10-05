df7cc0fbc80bd54b6b36c20fb39d187697a5899a
Ruslan Kibets <ruslan.kibets@gmail.com>
2020-10-05 04:04:54 -0700

    Impact Sounds (could be loud). Can't use far_explosion for some reason.

--- Assets/Scripts/SmolExplosion.cs
+++ Assets/Scripts/SmolExplosion.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 public class SmolExplosion : MonoBehaviour
 {
+    public AudioClip impactSound;
+
+    private void Start()
+    {
+        if (impactSound != null) AudioSource.PlayClipAtPoint(impactSound, transform.position);
+    }
+
     public void Die(float theValue)
     {
         Destroy(gameObject);
