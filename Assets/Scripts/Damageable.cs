9f729c79cd7044772c65fb8c2ef7136178fb266d
A333 <arz.giyan@gmail.com>
2020-10-05 11:55:03 +0300

    Turret hurt by explosions a little

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 public class Damageable : MonoBehaviour
 {
     public float Health = 20;
+    
+    [Range(0f, 1f)]
+    public float AOECoefficient = 1f;
+
     public float MaxHealth = 20;
 
     public event Action OnDie;
@@ -22,12 +26,12 @@ public class Damageable : MonoBehaviour
         Health = MaxHealth;
     }
 
-    public void Damage(GameObject who, float amount)
+    public void Damage(GameObject who, float amount, bool aoe = false)
     {
         OnHit?.Invoke();
 
         Debug.Log($"Damaging {gameObject.name} with {who.name} by {amount}.");
-        Health -= amount;
+        Health -= aoe ? amount * AOECoefficient : amount;
         if (Health <= 0)
         {
             OnDie?.Invoke();
